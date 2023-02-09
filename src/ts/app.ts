34844e9478e54bf5f7728ebc9398d66e1d238d44
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-02-09 09:58:31 +0100

    Add changes

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -12,48 +12,8 @@ const randomRecipeURL: string = "https://www.themealdb.com/api/json/v1/1/random.
 const mealCategories: string = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
 // ========== Functions to prevent DRY ==========
-
-
-// ========== Search button that takes user input and looks through an API ==========
-// TODO: När jag skriver något som inte finns, ex. "Test" får jag error för att forEach kan inte läsa "null". Hur fixar jag det?
-searchBtn.addEventListener("click", async (event) => {
-    event.preventDefault()
-    mealSearchResults.innerHTML = '';
-    
-    const res = await fetch(searchURL + inputField.value);
-    const data = await res.json();
-    searchHits.innerHTML =`
-    <h3 id="search-heading">${data.meals.length} matching results</h3>
-    `;
-    const historyBtn = document.createElement('button');
-    historyBtn.className = "history-btn";
-    historyBtn.innerText = inputField.value;
-    searchHistory.append(historyBtn);
-    historyBtn.addEventListener("click", async (event) => {
-        mealSearchResults.innerHTML = '';
-    
-        const res = await fetch(searchURL + historyBtn.innerText);
-        const data = await res.json();
-        searchHits.innerHTML =`
-        <h3 id="search-heading">${data.meals.length} matching results</h3>
-        `;
-        data.meals.forEach(meal => {
-            const mealHTML = `
-            <article class="meal-card ${meal.strMeal}">
-            <img class="meal-img" src="${meal.strMealThumb}" />
-            <div class="like not-liked"></div>
-            <div class="meal-info">
-            <h3 class="meal-name">${meal.strMeal}</h3>
-            <a href="url" class="meal-category">${meal.strCategory}</a>
-            </div>
-            </article>
-            `;
-            mealSearchResults.insertAdjacentHTML('afterbegin', mealHTML);
-        });
-    });
-
-
-    data.meals.forEach(meal => {
+const loadMealCards = function (arr) {
+    arr.forEach(meal => {
         const mealHTML = `
         <article class="meal-card ${meal.strMeal}">
         <img class="meal-img" src="${meal.strMealThumb}" />
@@ -65,24 +25,63 @@ searchBtn.addEventListener("click", async (event) => {
         </article>
         `;
         mealSearchResults.insertAdjacentHTML('afterbegin', mealHTML);
-        console.log(data.meals[0]);
-
-
-        // TODO: Fixa den här funktionen. Det går att toggla, men varannan kort har ett ojämt nummer och togglar en gång mer än föregående. Vad beror det på?
-        function listenForLikes () {
-            const likes = document.querySelectorAll(".like");
-            likes.forEach(like => {
-                like.addEventListener("click", (event) => {
-                    console.log("You clicked");
-                    (event.target as HTMLElement).classList.toggle("not-liked");
-                    (event.target as HTMLElement).classList.toggle("liked");
-                })
-            })
-        }
-        listenForLikes();
-
     });
+}
+
+
+// ========== Search button that takes user input and looks through an API ==========
+// TODO: När jag skriver något som inte finns, ex. "Test" får jag error för att forEach kan inte läsa "null". Hur fixar jag det?
+searchBtn.addEventListener("click", async (event) => {
+    event.preventDefault()
+    mealSearchResults.innerHTML = '';
+    
+    // ===== Fetches data from an API with user input as search value =====
+    const res = await fetch(searchURL + inputField.value);
+    const data = await res.json();
+    console.log(data.meals);
+    // ===== Creates a heading showing amount of hits on the search =====
+    searchHits.innerHTML =`
+    <h3 id="search-heading">${data.meals.length} matching results</h3>
+    `;
+    
+    // ===== Creates a search history button with the input value its innerText =====
+    const historyBtn = document.createElement('button');
+    historyBtn.className = "history-btn";
+    historyBtn.innerText = inputField.value;
+    searchHistory.append(historyBtn);
+    
+    // ===== Added event listener to the history button to fetch and load the content =====
+    historyBtn.addEventListener("click", async (event) => {
+        mealSearchResults.innerHTML = '';
+    
+        const res = await fetch(searchURL + historyBtn.innerText);
+        const data = await res.json();
+        searchHits.innerHTML =`
+        <h3 id="search-heading">${data.meals.length} matching results</h3>
+        `;
+        loadMealCards(data.meals);
+        listenForLikes();
+    });
+    
+    loadMealCards(data.meals);
+    listenForLikes();
 });
+
+
+function listenForLikes () {
+    const likes = document.querySelectorAll(".like") as NodeListOf<HTMLElement>;
+    for (const like of likes) {
+        like.addEventListener("click", function () {
+            if(this.classList.contains('not-liked')) {
+                this.classList.remove('not-liked')
+                this.classList.add('liked')
+            } else {
+                this.classList.add('not-liked')
+                this.classList.remove('liked')
+            }
+        })
+    }
+};
 
 
 // ========== Funktion för att ladda ett random recipe, knapp för att slumpa igen ==========
@@ -104,9 +103,5 @@ async function randomRecipe () {
         </article>
     `;
     randomContainer.insertAdjacentHTML('beforeend', randomInfo);
-
-
-    console.log(data.meals[0]);
-}
-
+};
 randomRecipe();
